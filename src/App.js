9d1b6cc793c57109267e35b70faa2dfdb35e32be
paulhenryk <kajfaszph@gmail.com>
2021-09-22 20:14:46 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import TodoList from './components/TodoList'
+
+function App() {
+  return (
+    <>
+      <h1>Todolist App</h1>
+      <h2>You can add a Todo by hitting the button</h2>
+      <TodoList/>
+    </>
+  )
+
+}
+
+export default App;
+
---
+++ src/components/TodoList.js
@@ -0,0 +1,42 @@
+
+import Todo from './Todo'
+import {useState,useRef} from 'react'
+
+const Todolist = () => {
+
+  const [list,setList] = useState([])
+
+  const formRef = useRef()
+
+  const addTodoHandler = (e) => {
+    e.preventDefault()
+    const formData = formRef.current
+    const newTodo = { 
+                      title: formData['title'].value,
+                      des: formData['des'].value
+    }
+
+    setList( oldArray =>  [...oldArray, newTodo] )
+  }
+
+  return (
+  <>
+    <ul>
+      {list.map(todo => <Todo title={todo.title} des={todo.des} />)}
+    </ul>
+
+    <form ref={formRef}>
+      <label for='title'>Todo title: </label> 
+      <input type='text' name='title' id='title' placeholder='title'/>
+      <br/>
+      <label for='des'>Todo description: </label> 
+      <input type='text' name='des' id='des' placeholder='des'/>
+      <br/>
+      <button onClick={addTodoHandler}>add Todo</button>
+    </form>
+  </>
+  )
+}
+
+export default Todolist
+
